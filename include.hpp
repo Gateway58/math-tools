4ace0edaf8eb49685e1e0e83f5613687608a0931
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-04-23 19:25:29 +0200

    Update include.hpp

--- include.hpp
+++ include.hpp
@@ -1,1 +1,15 @@
+#pragma once 
 
+#include <iostream>
+#include <math.h>
+#include <fstab.h>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <condition_variable>
+#include <cerrno>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
