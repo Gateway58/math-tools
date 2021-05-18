0c47185eb33a7d3ad95ff3048f95d99eb1adc91e
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-05-18 22:01:28 +0200

    More formulas
    added the formula F *∆t  = m * ∆v
    I hope it works

--- Physics/Fall and movement formula.cpp
+++ Physics/Fall and movement formula.cpp
@@ -14,33 +14,39 @@
 #include <istream>
 
 using namespace std; 
-//enter this to your desire 
-
+/*
+Update 18.05.2021 (19.05.2021) are marked with #a80505 Text gets red, when you install an extension that does it (I don't know the name right now)
+if you want to use ∆, just type in the given values but no more, just like you would do with others.I am already working on a better way to fix it, probably coming tomorrow or in 2 days. 
+So only change the important values to the importantest to figure out, not even gravity! 
+by troubles, please contact me at the given contacts from my github.
+enter this to your desire 
+*/
+//if you don't know this value, set it to 0
 //g usually 9.81 on earth, but if you are fancy, you can change this. 
-double gravity = 9.81;  
+long double gravity ;  
 
 // little t in s 
-double ttime= 0; //tt because c++ doesn't allow a variable named time 
+long double ttime = 0; //tt because c++ doesn't allow a variable named time 
 
-// little a (meter / second²) if you need to convert, then convert it yourself
-double acceleration = 0; 
+// little a (meter / second²)if you need to convert, then you can use my converter in the same folder and repository! called converter.cpp (Not out yet, its in the OddFormat Folder)
+long double acceleration = 0; 
 
-//little v (meter /second )if you need to convert, then convert it yourself
-double velocity = 0;
+//little v (meter /second )if you need to convert, then you can use my converter in the same folder and repository! called converter.cpp (Not out yet, its in the OddFormat Folder)
+long double velocity = 55.55556;
 
 //little h in meter
-double height = 20 ; 
+long double height = 0 ; 
 
 //big F in Newton 
-double Force = 0; 
+long double Force = 300; 
 
 //little s in meter 
-double determination = 0; 
+long double determination = 0; 
 
 //little m in kilogram   
-double mass = 0; 
+long double mass = 0.05; 
 //enter in here, what things to check. By more than 1 thing chang it through a semicolon (;)
-string WhatDoYouWant = " t; v  "; //Example: I want a and I want F: a; F
+string WhatDoYouWant = "   t  "; //Example: I want a and I want F: a; F
 //pro tip: you can chain. Which means, when you need t and want to get v with 
 //the t value, you don't have to get the result and chang it above in ttime, 
 //just type in (in this Example) t; v and it will auto-use the t value for v and 
@@ -55,7 +61,7 @@ char DeleteSpaces(string Search);
 int CheckWhatsThere(char WhatYouWantToDo);    
 //type the short things here. If you want 2 or more things, enter with a semicolon (;) 
 int Check;  
-double temp;  
+long double temp;  
 int main()
 {
    const int size = WhatDoYouWant.size()+1; 
@@ -80,7 +86,7 @@ for (int i = 0; i < size; i++)
                 cout << "a= " << temp<< endl; break; 
 
                 case 4: temp = Force /mass; acceleration = temp; 
-                cout << "a=" << temp << endl; break; 
+                cout << "a= " << temp << endl; break; 
                 default: break; 
             } 
             break; // End a 
@@ -97,6 +103,10 @@ for (int i = 0; i < size; i++)
             cout << "t= " << temp << endl;  break; 
             case 6: temp = sqrt((2*height/gravity)); ttime = temp; 
             cout << "t= " << temp << endl; break; 
+            //Start #a80505 (red) 
+            case 10: temp = mass*velocity/Force; mass = temp; 
+            cout << "t= " << temp << endl; break; 
+            //End #a80505 (red) 
         
         default: break;
         } 
@@ -113,6 +123,10 @@ for (int i = 0; i < size; i++)
             cout << "v= " << temp << endl;  break; 
             case 7: temp = sqrt((2*gravity*height)); velocity = temp; 
             cout << "v= " << temp << endl; break; 
+            //Start #a80505 (red) 
+            case 10: temp = Force*ttime/mass; mass = temp; 
+            cout << "v= " << temp << endl; break; 
+            //End #a80505 (red) 
         
         default: break;
         } 
@@ -155,7 +169,7 @@ for (int i = 0; i < size; i++)
             cout << "g= " << temp<< endl; break; 
 
             case 8: temp = Force /mass; gravity = temp; 
-            cout << "g=" << temp << endl; break; 
+            cout << "g= " << temp << endl; break; 
                 
 
         default: break;
@@ -170,11 +184,36 @@ for (int i = 0; i < size; i++)
       cout <<"F= " << temp << endl; break; 
       case 8: temp = mass*gravity; Force = temp; 
       cout <<"F= " << temp << endl; break; 
-      
+      //Start #a80505 (red) 
+      case 10: temp = mass*velocity/ttime; mass = temp; 
+      cout << "F= " << temp << endl; break; 
+      //End #a80505 (red) 
       default: break;
       }
       break;
       //End F
+      //Begin m and also #a80505 (red) 
+       case 'm': 
+      Check = CheckWhatsThere('m'); 
+      switch (Check)
+      {
+      case 4: temp = Force/acceleration; mass = temp; 
+      cout <<"m= " << temp << endl; break; 
+      case 8: temp = Force/gravity; mass = temp; 
+      cout <<"m= " << temp << endl; break; 
+      //Where #a80505 (red) should have started 
+      case 10: temp = Force*ttime/velocity; mass = temp; 
+      cout <<"m= " << temp << endl; break; 
+      //Where #a80505 (red) should have ended 
+      default: break;
+      }
+      break;
+      //End m 
+
+
+
+      default: break; 
+      //End #a80505 (red). I forgot m last time and to add a break when none of these match.
   }
    
    
@@ -185,8 +224,10 @@ for (int i = 0; i < size; i++)
 }
 string FindOutWhatYouWant()
 {
-
+     //cout << WhatDoYouWant << endl; 
     int pinpoint = WhatDoYouWant.find(";");
+   // cout << WhatDoYouWant << endl; 
+    //cout << pinpoint << endl; 
   if(pinpoint == -1)
   { string temps = WhatDoYouWant ; 
   WhatDoYouWant =""; 
@@ -195,6 +236,7 @@ string FindOutWhatYouWant()
      
     string output = WhatDoYouWant.substr(0, pinpoint); 
    WhatDoYouWant = WhatDoYouWant.substr(pinpoint+1);
+  // cout << WhatDoYouWant << endl;  
     return output; 
     
     
@@ -234,10 +276,16 @@ case 'v': if(acceleration != 0 && ttime != 0)return 1;
           if(acceleration != 0 && determination != 0)return 3; 
           if (gravity != 0 && ttime != 0)return 5; 
           if (gravity != 0 && height != 0)return 6; 
+          //Start #a80505 (red) 
+          if(Force != 0 && ttime != 0 && mass != 0)return 10; 
+          //End #a80505 (red) 
 
 case 't': if(velocity != 0 && acceleration != 0)return 1; 
           if(determination != 0 && acceleration != 0) return 2; 
           if(velocity != 0 && gravity != 0)return 5; 
+        //Start #a80505 (red) 
+          if(Force != 0 && velocity != 0 && mass != 0)return 10; 
+        //End #a80505 (red) 
           if(height != 0 && gravity != 0)return 6;  
 case 's': if(acceleration != 0 && ttime != 0)return 2; 
           if(acceleration != 0 && determination != 0)return 3; 
@@ -252,6 +300,15 @@ case 'g':
 case 'F': 
         if(mass != 0 && acceleration != 0) return 4; 
         if(mass != 0 && gravity != 0 ) return 8; 
+      //Start #a80505 (red) 
+        if(velocity != 0 && ttime != 0 && mass != 0)return 10; 
+      //End #a80505 (red) 
+case 'm': 
+        if(Force != 0 && acceleration != 0)return 4; 
+        if(Force != 0 && gravity != 0)return 8; 
+        //Start #a80505 (red) 
+        if(Force != 0 && ttime != 0 && velocity != 0)return 10; 
+        //End #a80505 (red)
 default: break; 
 }
 return 0; 
@@ -264,5 +321,7 @@ return 0;
 3: v² = 2*a*s          | 7: v²= 2*g*h
 4: F = m*a             | 8: F = m*g
 
-
+Start Update #a80505 (red) 
+10: F * ∆t = m * ∆v
+End Update #a80505 (red) 
 */
